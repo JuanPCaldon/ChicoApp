1d9e424b99c5d9c9c4b1c0d00068cb4876da065c
JuanPCaldon <122814773+JuanPCaldon@users.noreply.github.com>
2024-06-09 20:50:17 -0500

    login2

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,23 +3,22 @@ import { Link } from 'react-router-dom'
 import './LoginPage.css'
 export const LoginPage = () => {
   return (
-    <div>
-         <section>
-                <h1> Chico</h1>
-
-         </section>
-           <main >
-                <form action="">
-                         <h1>Iniciar Sesion </h1>
-                    <input type='text' placeholder="Email o numero de telefono"/>
-                    <input type='password' placeholder='Contraseña'/>
-                    <button type='submit'> <Link to="/Landing"> Iniciar sesion </Link></button>
-                    <div className='pn-container-2'>
-                        <span>O</span>
-                        <button type='submit'>Usar un codigo de inicio de sesion</button>
-                        <span>¿Olvidaste contraseña?</span>
+    <div className='pn-container-principal' >
+    
+           <main> 
+             
+                <form action="" >
+                    <img src="./Recurso_12x.png" alt="chico" />
+                        <h2>Iniciar Sesion </h2>
+                          <input type='text' placeholder="Email o numero de telefono"/>
+                          <input type='password' placeholder='Contraseña'/>
+                          <button type='submit'> <Link to="/Landing"> Iniciar sesion </Link></button>
+                            <div className='pn-container-2'>
+                                 <span> ¿Aun no tienes cuenta?</span>
+                                      <button type='submit'>Registro</button>
+                                 <span>¿Olvidaste contraseña?</span>
                        
-                    </div>
+                            </div>
 
 
                 </form>
